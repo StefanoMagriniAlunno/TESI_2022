eabdb3da9cd1801a677b2501e2a63511f7a18e75
StefanoMagriniAlunno <99906514+StefanoMagriniAlunno@users.noreply.github.com>
2022-04-11 12:14:06 +0200

    Add files via upload

---
+++ hdr/colouring.h
@@ -0,0 +1,8 @@
+#ifndef COLOURING_H
+#define COLOURING_H
+
+#include <stdlib.h>
+
+void colouring( const char* name_file_in_A, const char* name_file_in_B, const char* name_file_out_A, const size_t SIZE_GRAM);
+
+#endif	
---
+++ hdr/evidence.h
@@ -0,0 +1,8 @@
+#ifndef EVIDENCE_H
+#define EVIDENCE_H
+
+#include <stdlib.h>
+
+void evidence(const char* name_file,const size_t SIZE_GRAM);
+
+#endif	
